338fd90a53bce1d00e91d4025a3c0b8b44039635
steven <johanisteven123@gmail.com>
2025-12-11 17:38:48 -0500

    bugs base de datos

--- app/models.py
+++ app/models.py
@@ -7,8 +7,8 @@ db = SQLAlchemy()
 class Empresa(db.Model):
     __tablename__ = "empresa"
 
-    id_empresa = db.Column(db.String(20), primary_key=True, autoincrement=True)
-    nit = db.Column(db.Integer, nullable=False)
+    id_empresa = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nit = db.Column(db.String(20), nullable=False)
     nombre = db.Column(db.String(100), nullable=False)
     correo_electronico = db.Column(db.String(100), nullable=False)
     telefono_contacto = db.Column(db.String(20), nullable=False)
